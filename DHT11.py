56deb7edb77bf28fc56a66351ffee75b9bb12f61
z5308193 <z5308193@ad.unsw.edu.au>
2022-03-22 09:44:25 +1100

    First commit of basic Temp&Humidity Sensor Program

---
+++ DHT11.py
@@ -0,0 +1,21 @@
+import sys
+import time
+from datetime import datetime
+import Adafruit_DHT
+
+# set type of the sensor
+sensor = 11
+# set pin number
+pin = 4
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+
+while True:
+    timestamp = datetime.now().replace(microsecond=0)
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        print('{}: Temp={}C, Humidity={}%'.format(timestamp, temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+    time.sleep(2)
